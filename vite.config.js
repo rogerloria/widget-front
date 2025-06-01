3b8a048204b086723f6800b6501936af692627b1
rogerloria <rgrloria@gmail.com>
2025-05-31 21:12:33 -0600

    Ajuste

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,31 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: "./src/mountWidget.jsx", // <- el archivo que monta el widget automáticamente
+      entry: resolve(__dirname, "src/mountWidget.jsx"),
       name: "WhatsAppWidget",
       fileName: "widget",
       formats: ["iife"],
     },
+    rollupOptions: {
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    define: {
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    },
   },
 });
-
